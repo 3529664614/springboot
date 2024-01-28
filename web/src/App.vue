d01c279481c7f31e3b7fe21424d640c2092616b0
心怀憧憬 <9282104+beautifulseven@user.noreply.gitee.com>
2024-01-28 14:04:47 +0800

    创建地图和障碍物

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,39 +1,23 @@
 <template>
-  <body>
-    <div>Bot_name {{ bot_name }}</div>
-    <div>Bot_rating {{ bot_rating }}</div>
-  <router-view/>
-  </body>
+  <NavBar />
+  <router-view></router-view>
 </template>
 
-<style>
-  body{
-    background-image:url("./assets/background.jpg");
-    background-size: cover;
-  }
-</style>
-
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
+
 export default {
-  name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-    $.ajax({
-      url: "http://127.0.0.1:3000/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        console.log(resp);
-        bot_name.value = resp[0].name;
-        bot_rating.value = resp[0].rating;
-      }
-    });
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar
   }
 }
 </script>
+
+<style>
+body {
+  background-image: url("./assets/images/background.jpg");
+  background-size: cover;
+}
+</style>
